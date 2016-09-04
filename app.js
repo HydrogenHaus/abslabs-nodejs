cb4dc9eb7dc95a5d8a692c9aa6e71267189ff653
Brian Tovar <betovar@Brians-MacBook.lan>
2016-09-04 12:24:46 -0400

    minimal one-color and random working through web route, also env variables

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 const dgram = require('dgram');
 var app = express();
 
-const ABS_PORT = 6038;
-const ABS_IP_ADDR = '10.1.1.210'; //process.env.ABS_IP_ADDR;
+const ABS_PORT = process.env.ABS_PORT;
+const ABS_IP = process.env.ABS_IP;
 const header = [0x04, 0x01, 0xdc, 0x4a, 0x01, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0x00];
 const state = [0,0,0];
 //const data = new Buffer.from(state.buffer);
@@ -15,10 +15,6 @@ app.use(express.static('node_modules/jquery/dist/'));
 app.use(express.static('node_modules/bootstrap/dist/'));
 app.use(express.static('node_modules/angular/'));
 
-app.get('/', function(req, res) {
-  resp.render('home');
-});
-
 app.get('/v1/rand', function(req, res) {
   var i=0;
   var color = {
@@ -26,26 +22,29 @@ app.get('/v1/rand', function(req, res) {
     'g': Math.floor((Math.random() * 255)),
     'b': Math.floor((Math.random() * 255)),
   };
-  while (i <= state.length) {
+  while (i < state.length) {
     state[i++]=color.r;
     state[i++]=color.g;
     state[i++]=color.b;
   }
   send();
-  res.send(JSON.stringify(req.query));
+  res.send(JSON.stringify(color));
 });
 
 app.get('/v1/color', function(req, res) {
   //resp.send('color');
   var i=0;
-  while (i <= state.length) {
+  while (i < state.length) {
     state[i++]=req.query.r||0;
     state[i++]=req.query.g||0;
     state[i++]=req.query.b||0;
   }
-  console.log(state);
   send();
   res.send(JSON.stringify(req.query));
+});
+
+app.get('/', function(req, res) {
+  resp.render('home');
 });
 
 app.listen(app.get('port'), function() {
@@ -54,9 +53,8 @@ app.listen(app.get('port'), function() {
 
 function send() {
   var client = dgram.createSocket('udp4');
-  var d = new Buffer.from(header.concat(state));
-  console.log(d);  
-  client.send(d, 0 , d.byteLength, ABS_PORT, ABS_IP_ADDR, function(err, bytes){
+  var d = new Buffer.from(header.concat(state));  
+  client.send(d, 0 , d.length, ABS_PORT, ABS_IP, function(err, bytes){
     if (err) throw err;
     client.close();
   });
@@ -81,23 +79,4 @@ function send() {
 // }, function(err) {
 //   serial.write(data);
 //   serial.close();
-// });
-
-// const server = dgram.createSocket('udp4');
-// server.on('error', (err) => {
-//   console.log(`server error:\n${err.stack}`);
-//   server.close();
-// });
-// server.on('message', (msg, rinfo) => {
-//  const m = msg.toString();
-//   console.log(`message: ${m}`);
-// });
-// server.on('listening', () => {
-//   var address = server.address();
-//   console.log(`server listening ${address.address}:${address.port}`);
-// });
-// server.bind({
-//   address: 'localhost',
-//   port: PORT,
-//   exclusive: true
 // });
